a559ac804dbc93b52db869bfc6d399d66083ce8b
Agesh <macbeth@Ageshs-MacBook-Air.local>
2026-07-06 10:36:13 +0530

    hosts

--- core/settings.py
+++ core/settings.py
@@ -29,9 +29,11 @@ SECRET_KEY = 'django-insecure-%ilp)w9ac$@o^ykjt#mt3$fmbdt1zhnl@7h3mv&^!9f^2#s6re
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
-
+ALLOWED_HOSTS = [
+    "erpbackend-production-0d19.up.railway.app",
+    "localhost",
+    "127.0.0.1",
+]
 # Application definition
 
 INSTALLED_APPS = [
